14d0225568634526ae584348bee19721551705f6
javier-cantu <pacocantu87@gmail.com>
2024-05-16 19:05:16 -0600

    First commit! Exercise 001 Rust

---
+++ src/bin/001_exercise.rs
@@ -0,0 +1,10 @@
+// Exercise 001 - Hello World
+
+// The `main` function is the entry point of a Rust program. 
+fn main() {
+    // The `println!` macro prints text to the console.
+    // Macros in Rust end with an exclamation mark `!`.
+    // Here, `println!` takes a string as an argument and prints it, followed by a newline.
+    println!("Exercise 001");
+    println!("Hello World!");
+}
